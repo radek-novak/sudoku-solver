472d6ea516ab3d79ff86562efd0d89e5b9e1e2ec
Radek Novak <radek.novak.cze@gmail.com>
2018-08-23 11:07:26 +0200

    fix test warnings

--- sudoku_test.go
+++ sudoku_test.go
@@ -6,14 +6,14 @@ import (
 
 func TestCheckNine(t *testing.T) {
 	t.Run("check valid", func(t *testing.T) {
-		isValid, isSolved := CheckNine([]int8{0, 0, 1, 2, 3, 4, 0, 5, 6})
+		isValid, isSolved := CheckNine([...]int8{0, 0, 1, 2, 3, 4, 0, 5, 6})
 		if !isValid || isSolved {
 			t.Errorf("Should've been invalid and not solved but was %t and %t", isValid, isSolved)
 		}
 	})
 
 	t.Run("check solved", func(t *testing.T) {
-		isValid, isSolved := CheckNine([]int8{1, 2, 3, 4, 7, 8, 9, 5, 6})
+		isValid, isSolved := CheckNine([...]int8{1, 2, 3, 4, 7, 8, 9, 5, 6})
 
 		if !isValid || !isSolved {
 			t.Errorf("Should've been valid and solved but was %t and %t", isValid, isSolved)
@@ -21,7 +21,7 @@ func TestCheckNine(t *testing.T) {
 	})
 
 	t.Run("check false", func(t *testing.T) {
-		isValid, isSolved := CheckNine([]int8{1, 2, 3, 4, 7, 8, 9, 5, 4})
+		isValid, isSolved := CheckNine([...]int8{1, 2, 3, 4, 7, 8, 9, 5, 4})
 
 		if isValid || isSolved {
 			t.Errorf("Should've been not valid and not solved but was %t and %t", isValid, isSolved)
