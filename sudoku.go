3f64bf6449cb8493650a935fc925f30b52d6b131
Radek Novak <radek.novak.cze@gmail.com>
2018-08-28 11:07:54 +0200

    attempt at concurrency

--- sudoku.go
+++ sudoku.go
@@ -1,24 +1,46 @@
 package main
 
-func main() {
-	board := ReadInput()
-	queue := []Board{}
-	queue = append(queue, board)
+import (
+	"fmt"
+	"os"
+	"sync/atomic"
+)
 
-	for {
-		lastIx := len(queue) - 1
-		currentBoard := queue[lastIx]
-		isValid, isSolved := CheckBoard(currentBoard)
-
+func worker(boardToSolve <-chan Board, nextBoardsChan chan<- []Board) {
+	for board := range boardToSolve {
+		isValid, isSolved := CheckBoard(board)
 		if isSolved {
-			PrettyPrint(currentBoard)
-			return
+			PrettyPrint(board)
+			os.Exit(0)
+		}
+		if isValid {
+			nextBoardsChan <- GetNextBoards(board)
+		}
+	}
+}
+
+func main() {
+	var ops int32
+
+	board := ReadInput()
+	boardToSolve := make(chan Board, 49900)
+	nextBoardsChan := make(chan []Board, 300)
+
+	for w := 0; w < 4; w++ {
+		go worker(boardToSolve, nextBoardsChan)
+	}
+
+	boardToSolve <- board
+
+	for nextBoards := range nextBoardsChan {
+		atomic.AddInt32(&ops, 1)
+		i := atomic.LoadInt32(&ops)
+		if i%100 == 0 {
+			fmt.Println(i)
 		}
 
-		queue = queue[:lastIx]
-
-		if isValid {
-			queue = append(queue, GetNextBoards(currentBoard)...)
+		for _, board := range nextBoards {
+			boardToSolve <- board
 		}
 	}
 }
