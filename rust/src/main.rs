593caa472b1c4cafa9c8e54ee8805b34a33d5b11
Radek Novak <radek.novak.cze@gmail.com>
2020-11-14 16:22:59 +0100

    rust: stat printing

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -19,15 +19,24 @@ fn main() {
     let parsed_board = sudoku::parse_board(contents);
     let initial_board = sudoku::Board::new(parsed_board);
 
-    // initial_board.print_board();
+    println!("Puzzle:");
+    initial_board.print_board();
+    let zero_count = parsed_board.iter().fold(0, |acc, num| {
+        if *num == 0 {
+            return acc + 1;
+        }
+        return acc;
+    });
+    println!("Clues: {}", 81 - zero_count);
 
     let mut queue = vec![initial_board];
     let mut current = queue[0];
+    let mut max_queue_len: usize = queue.len();
+    let mut boards_checked: u128 = 0;
 
     loop {
         if current.valid_board() {
             let next_boards = current.get_next_boards();
-
             match next_boards {
                 Some(nb) => {
                     for next_board in nb {
@@ -35,7 +44,11 @@ fn main() {
                     }
                 }
                 None => {
+                    println!("Solution:");
                     current.print_board();
+                    println!("checked {} boards", boards_checked);
+                    println!("max queue length was: {}", max_queue_len);
+
                     break;
                 }
             }
@@ -51,6 +64,13 @@ fn main() {
                 // the program is incorrect.
                 panic!("Solving error, puzzle is invalid");
             }
+        };
+
+        let current_len = queue.len();
+        if current_len > max_queue_len {
+            max_queue_len = current_len;
         }
+
+        boards_checked += 1;
     }
 }
